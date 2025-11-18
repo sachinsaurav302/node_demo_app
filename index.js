c52e42538632055bb03498ed1d3a04de5eeeca91
chdeepakops <chdeepak1991@gmail.com>
2025-11-18 15:51:12 +0530

    Add dependabot config for npm and github-actions

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Hello Dependabot Demo'));
+app.listen(3000, () => console.log('Server running on 3000'));
